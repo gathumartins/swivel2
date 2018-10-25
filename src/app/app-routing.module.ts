123827331a30e193e0b07a316c96a02d5d0fdd51
gathumartins <gathumartins@gmail.com>
2018-10-25 11:06:06 +0300

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { from } from 'rxjs';
+import { HomeComponent } from './home/home.component';
+import { EnterpriseComponent } from './enterprise/enterprise.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {path: 'home', component: HomeComponent},
+  {path: 'enterprise', component: EnterpriseComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar.component';
+import { FooterComponent } from './footer.component';
+import { HomeComponent } from './home/home.component';
+import { PartnersComponent } from './partners.component';
+import { EnterpriseComponent } from './enterprise/enterprise.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    HomeComponent,
+    PartnersComponent,
+    EnterpriseComponent
   ],
   imports: [
     BrowserModule,
